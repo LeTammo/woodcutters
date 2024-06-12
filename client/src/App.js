3fe3242a9c15febc838b502c239eb02a88171993
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-12 10:55:27 +0200

    Fix grid layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Room from './components/Room';
 import './styles/App.css';
@@ -13,20 +13,31 @@ function App() {
         }
     };
 
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            handleSetUsername();
+        }
+    };
+
     const [username, setUsername] = useState(sessionStorage.getItem('username'));
 
     if (!username) {
         return (
-            <div className="container col-xl-10 col-xxl-8 px-4 py-5">
+            <div className="container px-4 py-5">
                 <div className="row align-items-center g-lg-5 py-5">
-                    <div className="col-md-10 mx-auto col-lg-5">
+                    <div className="mx-auto col-sm-12 col-md-10 col-lg-6">
                         <div className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-                            <input
-                                type="text"
-                                className="form-control mb-2"
-                                placeholder="Benutzername"
-                            />
-                            <button className="btn btn-primary" onClick={handleSetUsername}>Benutzername setzen</button>
+                            <div className="input-group mb-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Wie möchtest du heißen?"
+                                    onKeyUp={handleKeyPress}
+                                />
+                                <button className="btn btn-success" onClick={handleSetUsername}>
+                                    Starten
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
