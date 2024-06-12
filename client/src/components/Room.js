ac44c4b801f1f83f0741b0deef50ea1e86f16c8a
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-12 10:35:48 +0200

    Remove unnecessary username code

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,49 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { socket } from '../socket';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import Game from './Game';
 
 function Room() {
     const { roomId } = useParams();
-    const [username, setUsername] = useState('');
-    const [isUsernameSet, setIsUsernameSet] = useState(false);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const savedUsername = sessionStorage.getItem('username');
-        if (savedUsername) {
-            setUsername(savedUsername);
-            setIsUsernameSet(true);
-            socket.emit('joinRoom', { roomId, username: savedUsername });
-        } else {
-            navigate('/username', { state: { redirectTo: `/${roomId}` } });
-        }
-    }, [roomId, navigate]);
-
-    const handleSetUsername = () => {
-        if (username.trim() !== '') {
-            sessionStorage.setItem('username', username);
-            setIsUsernameSet(true);
-            socket.emit('joinRoom', { roomId, username });
-        }
-    };
 
     return (
         <div className="container mt-3">
-            {!isUsernameSet ? (
-                <div>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        className="form-control mb-2"
-                        placeholder="Benutzername"
-                    />
-                    <button className="btn btn-primary" onClick={handleSetUsername}>Benutzername Setzen</button>
-                </div>
-            ) : (
-                <Game roomId={roomId} username={username} />
-            )}
+            <Game roomId={roomId} username={sessionStorage.getItem('username')} />
         </div>
     );
 }
