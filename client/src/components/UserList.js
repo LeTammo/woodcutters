fba34faf53bad990a837354da551949fd60daff2
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-12 12:23:20 +0200

    Remove player indicator

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -3,11 +3,11 @@ import React from 'react';
 function UserList({ users, orderStatus }) {
     return (
         <div>
-            <h2>Spieler:</h2>
+            <h4>Spieler in Lobby:</h4>
             <ul className="list-group">
                 {users.map((user, index) => (
                     <li key={index} className="list-group-item">
-                        {user.username} {orderStatus[user.username] ? '- Hat bestellt' : ''} ({user.role === 'player' ? 'Spieler' : 'Zuschauer'})
+                        {user.username} {orderStatus[user.username] ? '- Hat bestellt' : ''} {user.role !== 'player' && '(Zuschauer)'}
                     </li>
                 ))}
             </ul>
