363d422ee050d84e0234296e0539025405cfe508
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-13 19:37:26 +0200

    Change greeting message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -23,7 +23,7 @@ function Home() {
         <div className="row align-items-center g-lg-5 py-5">
             <div className="mx-auto col-sm-12 col-md-10 col-lg-6">
                 <div className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-                    <h5 className="text-center mb-5">Hallo {sessionStorage.getItem('username')} :)</h5>
+                    <h5 className="text-center mb-5">Hallo {sessionStorage.getItem('username')}!</h5>
                     <div>
                         <button className="btn btn-success" onClick={createRoom}>Raum erstellen</button>
                     </div>
