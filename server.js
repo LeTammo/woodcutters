cb6bdf431660f8382c221260ff25bfa656435055
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-15 10:39:33 +0200

    change server message

--- server.js
+++ server.js
@@ -269,5 +269,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
