cf76543cbef7035b75f5f9e1d3a1cc938dbc2a69
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-17 14:07:35 +0200

    Refactor App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { socket } from './socket';
 import Home from './components/Home';
 import Room from './components/Room';
+import RegisterUser from './components/RegisterUser';
 import './styles/App.css';
 
 function App() {
     const [playerId, setPlayerId] = useState(localStorage.getItem('playerId'));
     const [username, setUsername] = useState(localStorage.getItem('username'));
 
-    useEffect(() => {
-        const handlePlayerId = (id) => {
-            setPlayerId(id);
-            localStorage.setItem('playerId', id);
-        };
-
-        if (!playerId) {
-            socket.emit('requestPlayerId');
-            socket.on('playerId', handlePlayerId);
-        }
-
-        return () => {
-            socket.off('playerId', handlePlayerId);
-        };
-    }, [playerId]);
-
-    useEffect(() => {
-        if (playerId && username) {
-            socket.emit('registerUser', { playerId, username });
-        }
-    }, [playerId, username]);
-
-    const handleSetUsername = () => {
-        const input = document.querySelector('input');
-        const userName = input.value.trim();
-        if (userName !== '') {
-            setUsername(userName);
-            localStorage.setItem('username', userName);
-        }
-    };
-
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            handleSetUsername();
-        }
-    };
-
-    if (!username) {
-        return (
-            <div className="row align-items-center g-lg-5 py-5">
-                <div className="mx-auto col-sm-12 col-md-10 col-lg-6">
-                    <div className="p-4 p-md-5 border rounded-3 bg-opacity-92">
-                        <div className="input-group">
-                            <input
-                                type="text"
-                                className="form-control shadow-none"
-                                placeholder="Wie möchtest du heißen?"
-                                onKeyUp={handleKeyPress}
-                            />
-                            <button className="btn btn-success" onClick={handleSetUsername}>
-                                Starten
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home playerId={playerId} username={username} />} />
-                <Route path="/:roomId" element={<Room playerId={playerId} username={username} />} />
-            </Routes>
-        </Router>
+        !username || !playerId
+            ?
+            <RegisterUser playerId={playerId} setPlayerId={setPlayerId} username={username} setUsername={setUsername} />
+            :
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home playerId={playerId} username={username} />} />
+                    <Route path="/:roomId" element={<Room playerId={playerId} username={username} />} />
+                </Routes>
+            </Router>
     );
 }
 
---
+++ client/src/components/RegisterUser.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { socket } from "../socket";
+
+function RegisterUser({ playerId, setPlayerId, username, setUsername }) {
+    useEffect(() => {
+        const handlePlayerId = (id) => {
+            setPlayerId(id);
+            localStorage.setItem('playerId', id);
+        };
+
+        if (!playerId) {
+            socket.emit('requestPlayerId');
+            socket.on('playerId', handlePlayerId);
+        }
+
+        return () => {
+            socket.off('playerId', handlePlayerId);
+        };
+    }, [playerId]);
+
+    useEffect(() => {
+        if (playerId && username) {
+            socket.emit('registerUser', { playerId, username });
+        }
+    }, [playerId, username]);
+
+    const handleSetUsername = () => {
+        const input = document.querySelector('input');
+        const userName = input.value.trim();
+        if (userName !== '') {
+            setUsername(userName);
+            localStorage.setItem('username', userName);
+        }
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            handleSetUsername();
+        }
+    };
+
+    return (
+        <div className="row align-items-center g-lg-5 py-5">
+            <div className="mx-auto col-sm-12 col-md-10 col-lg-6">
+                <div className="p-4 p-md-5 border rounded-3 bg-opacity-92">
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control shadow-none"
+                            placeholder="Wie möchtest du heißen?"
+                            onKeyUp={handleKeyPress}
+                        />
+                        <button className="btn btn-success" onClick={handleSetUsername}>
+                            Starten
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterUser;
