9d783f0f0eddee0ea24d5990feb2edc00cd2e8b9
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-26 23:33:02 +0200

    Change code style again

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -37,35 +37,39 @@ function Game() {
         <div className="align-items-center g-lg-5 py-5">
             <div className="row p-4 p-md-5 border rounded-3 bg-body-tertiary bg-opacity-92">
                 <div className="col-12 col-sm-6 col-md-8">
-                    {!gameStarted ? (<>
-                        <ShareRoom />
-                        <PrepareGame isReady={isReady} handleReady={handleReady} />
-                        <Rules />
-                    </>) : (<>
-                        <RoundHistory
-                            roundHistory={roundHistory}
-                            users={connectedUsers}
-                            orderStatus={orderStatus}
-                            gameRunning={gameStarted && !gameEnded}
-                            gameEnded={gameEnded}
-                            trees={trees}
-                            order={order}
-                            message={message}
-                        />
-                        <GameControls
-                            gameStarted={gameStarted}
-                            role={role}
-                            isReady={isReady}
-                            handleReady={handleReady}
-                            hasOrdered={hasOrdered}
-                            gameEnded={gameEnded}
-                            trees={trees}
-                            order={order}
-                            setOrder={setOrder}
-                            placeOrder={placeOrder}
-                            message={message}
-                        />
-                    </>)}
+                    {!gameStarted ? (
+                        <>
+                            <ShareRoom />
+                            <PrepareGame isReady={isReady} handleReady={handleReady} />
+                            <Rules />
+                        </>
+                    ) : (
+                        <>
+                            <RoundHistory
+                                roundHistory={roundHistory}
+                                users={connectedUsers}
+                                orderStatus={orderStatus}
+                                gameRunning={gameStarted && !gameEnded}
+                                gameEnded={gameEnded}
+                                trees={trees}
+                                order={order}
+                                message={message}
+                            />
+                            <GameControls
+                                gameStarted={gameStarted}
+                                role={role}
+                                isReady={isReady}
+                                handleReady={handleReady}
+                                hasOrdered={hasOrdered}
+                                gameEnded={gameEnded}
+                                trees={trees}
+                                order={order}
+                                setOrder={setOrder}
+                                placeOrder={placeOrder}
+                                message={message}
+                            />
+                        </>
+                    )}
                 </div>
                 <div className="col-12 col-sm-6 col-md-4">
                     <div className="mt-3">
