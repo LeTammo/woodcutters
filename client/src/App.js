8bbb9d7281ff49fbc93cb1253e16e426a92476d2
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-06-26 20:32:40 +0200

    Clean code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider, useUser } from './context/UserContext';
 import RegisterUser from './components/RegisterUser';
 import Home from './components/Home';
